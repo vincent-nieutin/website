483c1eaab550425401ab175f00d54cbdb2b7795a
vincent <vincent.nieutin@yahoo.fr>
2023-05-12 13:14:53 -0400

    add localization

--- src/views/CVView.vue
+++ src/views/CVView.vue
@@ -1,39 +1,11 @@
 <script setup>
 import { reactive } from 'vue'
 import Item from '../components/CV/Item.vue'
-
-const items = [
-  {
-    type: 'professional-experience',
-    title: 'Développeur Full Stack',
-    company: 'OpenFly',
-    location: 'Guipavas',
-    startDate: '2021-10',
-    endDate: null,
-    description: [
-      "Maintenance et développement du site et de l'application de l'entreprise",
-      "Utilisation de GitLab, Laravel, VueJS, MySQL"
-    ]
-  },
-  {
-    type: 'professional-experience',
-    title: 'Stage cybersécurité',
-    company: 'Harvest',
-    location: 'Paris',
-    startDate: '2018-05',
-    endDate: '2018-08',
-    description: [
-      "Proof of Concept sur l'intégration d'un Hardware Security Module aux applications de l'entreprise afin de renforcer leur sécurité",
-      "Élaboration d'un système serveur/client et chiffrement des données échangées",
-      "Présentation du POC aux associés directeurs de l'entreprise"
-    ]
-  }
-]
-
 </script>
 
 <template>
   <div class="cv">
+    {{ $t('professional-experiences.openfly.title') }}
     <Item v-for="item in items" :item="item" />
   </div>
 </template>
