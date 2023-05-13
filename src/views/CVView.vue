a5ca284dd27ca4f0687ba76fef3567e5787d9197
vincent <vincent.nieutin@yahoo.fr>
2023-05-13 14:57:28 -0400

    cv categories display improvement

--- src/views/CVView.vue
+++ src/views/CVView.vue
@@ -6,11 +6,23 @@ import Item from '../components/CV/Item.vue'
 <template>
   <div class="cv">
     <!-- Proffession experience -->
-    <Item v-for="(item, index) in $i18n.messages[$i18n.locale]['professional-experiences']" :item="item" />
+    <div v-for="(key, index) in Object.keys($i18n.messages[$i18n.locale])">
+      <div class="category-title">{{ $i18n.messages[$i18n.locale][key].title.toUpperCase() }}</div>
+      <Item v-for="(item, index) in $i18n.messages[$i18n.locale][key].content" :item="item" />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.category-title {
+  width: 100%;
+
+  margin: 20px 0 20px 0;
+
+  font-weight: bold;
+  font-size: 25px;
+}
+
 .cv {
   display: flex;
   flex-direction: column;
