b28ebb3597c08717d65a954cdba9d4366e1c41e5
vincent <vincent.nieutin@yahoo.fr>
2023-05-16 12:49:48 -0400

    add aliases

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@views': '@/views',
+      '@assets': '@/assets',
+      '@components': '@/components',
     }
   }
 })
